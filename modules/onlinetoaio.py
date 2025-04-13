d12514dc25bbd6294744998b1be20857cc935e41
shaffay-aio <muhammad.shaffay@aioapp.com>
2025-04-13 22:09:16 +0500

    onlinetoaio module updated, middleware section functional

--- modules/onlinetoaio.py
+++ modules/onlinetoaio.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from modules.utils import download_file
+import time
 
 # Page 3: OnlinetoAIO
 def onlinetoaio_page():
@@ -7,22 +8,65 @@ def onlinetoaio_page():
     
     # Section 1: Scraped Middleware File
     st.header("Scraped Middleware File")
-    platform = st.selectbox("Select Platform", ["Toast", "UberEATS", "DoorDash"])
+    platform = st.selectbox("Select Platform", ["Doordash", "Ubereats", "Toast"])
     url = st.text_input("Enter URL")
     
+    # Submit button for Section 1
+    submit_section1 = st.button("Submit URL", key="section1_submit")
+
     # Section 2: Middleware to AIO
     st.header("Middleware To AIO")
     file = st.file_uploader("Upload Middleware File", type=["xlsx"])
+
+    # Submit button for Section 2
+    submit_section2 = st.button("Submit Middleware", key="section2_submit")
     
-    # Submit button
-    if st.button("Submit"):
-        if url and file:
-            files = {"file": file.getvalue()}
-            data = {"platform": platform, "url": url}
-            api_url = "http://yourapiurl.com/onlinetoaio"
-            file_content = download_file(api_url, files, data)
-            
-            if file_content:
-                st.download_button("Download Processed File", file_content, "processed_file.xlsx")
+    # State for processing flags
+    if 'section1_processing' not in st.session_state:
+        st.session_state.section1_processing = False
+    if 'section2_processing' not in st.session_state:
+        st.session_state.section2_processing = False
+
+    # Section 1 submit logic
+    if submit_section1 and not st.session_state.section1_processing:
+        if url:
+            st.session_state.section1_processing = True
+            with st.spinner("Processing Section 1..."):
+                files = {"file": file.getvalue()}
+                data = {"platform": platform, "url": url}
+                api_url = "http://yourapiurl.com/onlinetoaio"
+                file_content = download_file(api_url, files, data)
+                
+                if file_content:
+                    st.session_state.section1_processing = False
+                    st.download_button("Download Processed File Section 1", file_content, "processed_file_section1.xlsx")
+                else:
+                    st.session_state.section1_processing = False
+                    st.error("Error processing Section 1. Please try again.")
         else:
-            st.error("Please provide URL and upload a file.")
+            st.error("Please provide URL for Section 1.")
+    
+    # Section 2 submit logic
+    if submit_section2 and not st.session_state.section2_processing:
+        if file:
+            st.session_state.section2_processing = True
+            with st.spinner("Processing Section 2..."):
+                files = {"file": file.getvalue()}
+                data = {"platform": platform, "url": url}
+                api_url = "http://44.231.228.32:8040/onlinetoaioformatter"
+                file_content = download_file(api_url, files, data)
+                
+                if file_content:
+                    st.session_state.section2_processing = False
+                    st.download_button("Download Processed File Section 2", file_content, "aio-format.xlsx")
+                else:
+                    st.session_state.section2_processing = False
+                    st.error("Error processing Section 2. Please try again.")
+        else:
+            st.error("Please upload a file for Section 2.")
+    
+    # Disable other section's button during processing
+    if st.session_state.section1_processing:
+        st.session_state.section2_processing = True
+    if st.session_state.section2_processing:
+        st.session_state.section1_processing = True
